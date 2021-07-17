66455a700a800ca4671c641e47eec6076ad1d20b
Meer Estiyak <mmestiyak@gmail.com>
2021-07-17 05:33:31 -0700

    added comment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import App Component
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
